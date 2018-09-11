b6349ca557d2245432818ed2640703e623a8b319
chong <39102393+chongnou@users.noreply.github.com>
2018-09-10 23:24:00 -0500

    added contestant class

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        public string firstName;
+        public string lastName;
+        public int ticketNumber;
+        public string result;
+
+        public Contestant(string FirstName, string LastName, int TicketNumber, string Result)
+        {
+
+            firstName = FirstName;
+            lastName = LastName;
+            ticketNumber = TicketNumber;
+            result = Result;
+
+        }
+    }
+}
