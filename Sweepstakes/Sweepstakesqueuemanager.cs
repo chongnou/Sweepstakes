885ca9c02e3e1c2c95c44a790a88fc7266183426
chong <39102393+chongnou@users.noreply.github.com>
2018-09-10 23:20:57 -0500

    added sweepstakesqueuemanager class

---
+++ Sweepstakes/Sweepstakesqueuemanager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class SweepstakesQueueManager : ISweepstakesManager
+    {
+        Queue<Sweepstakes> sweepsQueue = new Queue<Sweepstakes>();
+
+        public void InsertSweepstakes(Sweepstakes sweepstake)
+        {
+            sweepsQueue.Enqueue(sweepstake);
+        }
+        public Sweepstakes GetNextSweepstakesWinner()
+        {
+            return sweepsQueue.Dequeue();
+        }
+    }
+}
